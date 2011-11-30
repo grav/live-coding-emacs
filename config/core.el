00b122f6ddaaf7f3327f2f9b4d942351007eacef
Mikkel Gravgaard <mikkel@klokke.dk>
2011-12-01 00:03:36 +0100

    support for package.el (emacs 23) and marmalade. package.el (elpa) packages will be loaded first, so for instance, a newer clojure-mode can be installed via package-install

--- config/core.el
+++ config/core.el
@@ -6,5 +6,11 @@
 (load-file (concat dotfiles-lib-dir "blackbored.el"))
 (color-theme-blackbored)
 
+(load-file (concat dotfiles-dir "package.el"))
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
 ;;Or load external files such as this bindings file:
 (load-dotfile "config/bindings.el")
